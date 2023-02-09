e02a9d648b5aee974a95c56916fa7b01f9a9258a
Jimmy <jimkiarie8@gmail.com>
2023-02-09 17:25:29 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    // BrowserAnimationsModule,
+    // MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CheckoutService } from 'src/app/services/checkout.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  qty = this.route.snapshot.paramMap.get('id');
 
+  form = {
+    qty: this.route.snapshot.paramMap.get('id')
+  };
+
+  price = 0;
+  total = 0;
+
+
+  items = [
+    {
+      qty: '1',
+      price: 4900,
+      total: 4900
+    },
+    {
+      qty: '3',
+      price: 2997,
+      total: 8990
+    },
+    {
+      qty: '5',
+      price: 2798,
+      total: 13990
+    }
+  ];
+
+  constructor(private route: ActivatedRoute, private checkoutService: CheckoutService) { }
+
+
+  ngOnInit() {
+    if (this.route.snapshot.paramMap.get('id') == '1') {
+      this.price = 4900;
+      this.total = 4900;
+    } else if (this.route.snapshot.paramMap.get('id') == '3') {
+      this.price = 2997;
+      this.total = 8990;
+    } else if (this.route.snapshot.paramMap.get('id') == '5') {
+      this.price = 2798;
+      this.total = 13990;
+    } else {
+      this.price = 4900;
+      this.total = 4900;
+    }
+  }
+  onSelectionChange(event: any) {
+    console.log(event.target.value);
+    this.items.forEach(element => {
+      if (this.form.qty === element.qty) {
+        this.price = element.price;
+        this.total = element.total;
+      }
+    });
+  }
+
+  checkout() {
+    this.checkoutService.checkout(this.form).subscribe((res) => {
+      console.log("🚀 ~ file: checkout.component.ts:69 ~ CheckoutComponent ~ this.checkoutService.checkout ~ res", res)
+    });
+  }
 }
