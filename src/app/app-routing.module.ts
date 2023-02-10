f8fceb22b0b3e34c5d228f59690a9ba375687961
dennohgitau <denisgitaustudy@gmail.com>
2023-02-10 09:16:51 +0300

    First commit on Friday

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { HomeComponent } from './components/home/home.component';
+import { SuccessComponent } from './components/success/success.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'checkout/:id', component: CheckoutComponent }
+  { path: 'checkout/:id', component: CheckoutComponent },
+  { path: 'success', component: SuccessComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { HomeComponent } from './components/home/home.component';
+import { SuccessComponent } from './components/success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CheckoutComponent,
-    HomeComponent
+    HomeComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
